1ef1c50741a26d6b522f8e87e7de70be094e8000
radiocontrolled <radiocontrolled@gmail.com>
2017-07-05 19:38:53 +0100

    my first website\!

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var formidable = require('express-formidable');
 
-app.listen(3000, () => {
-    console.log("Server running");
+// serve assets from /public dir
+app.use(express.static('public'));
+app.use(formidable());
+
+app.listen(3000, function() {
+
 });
+
+
+app.post('/create-post', function (req, res){
+  res.send('i\'m here');
+  console.log(req.fields);
+  // console.log(req.body);
+});
+
+
+
